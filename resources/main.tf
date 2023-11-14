c0b286f8ebd19f7bb3597809f959bdfcb3309064
tanvighai <tanvighai02@gmail.com>
2023-11-14 01:17:23 -0500

    test commit

--- resources/main.tf
+++ resources/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z062321418KWGB5HR8726"
+  name    = "test.tanvi12online.net"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip]
+}
