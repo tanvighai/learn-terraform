7f9da993fb0f44bf0257da7e2351a8f1ce11f633
tanvighai <tanvighai02@gmail.com>
2023-11-15 02:39:36 -0500

    test commit

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,56 +1,56 @@
-resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
-
-  tags = {
-    Name = "frontend"
-  }
-}
-
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z062321418KWGB5HR8726"
-  name    = "frontend.tanvi12online.net"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-
-resource "aws_instance" "backend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
-
-  tags = {
-    Name = "backend"
-  }
-}
-
-resource "aws_route53_record" "backend" {
-  zone_id = "Z062321418KWGB5HR8726"
-  name    = "backend.tanvi12online.net"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.backend.private_ip]
-}
-
-
-
-resource "aws_instance" "mysql" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z062321418KWGB5HR8726"
-  name    = "mysql.tanvi12online.net"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#}
+#
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z062321418KWGB5HR8726"
+#  name    = "frontend.tanvi12online.net"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
+#
+#
+#resource "aws_instance" "backend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
+#
+#  tags = {
+#    Name = "backend"
+#  }
+#}
+#
+#resource "aws_route53_record" "backend" {
+#  zone_id = "Z062321418KWGB5HR8726"
+#  name    = "backend.tanvi12online.net"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.backend.private_ip]
+#}
+#
+#
+#
+#resource "aws_instance" "mysql" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
+#
+#  tags = {
+#    Name = "mysql"
+#  }
+#}
+#
+#resource "aws_route53_record" "mysql" {
+#  zone_id = "Z062321418KWGB5HR8726"
+#  name    = "mysql.tanvi12online.net"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.mysql.private_ip]
+#}
