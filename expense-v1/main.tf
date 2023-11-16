56e0d4c0cb6257cd77551fff7260ea776353924e
tanvighai <tanvighai02@gmail.com>
2023-11-16 00:00:59 -0500

    test commit

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,13 +1,21 @@
 resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
   }
-}
 
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+EOF
+  }
+}
 resource "aws_route53_record" "frontend" {
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = "frontend.${var.zone_id}"
@@ -25,6 +33,14 @@ resource "aws_instance" "backend" {
   tags = {
     Name = "backend"
   }
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+EOF
+  }
 }
 
 resource "aws_route53_record" "backend" {
@@ -35,8 +51,6 @@ resource "aws_route53_record" "backend" {
   records = [aws_instance.backend.private_ip]
 }
 
-
-
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
@@ -44,6 +58,14 @@ resource "aws_instance" "mysql" {
 
   tags = {
     Name = "mysql"
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+EOF
   }
 }
 
