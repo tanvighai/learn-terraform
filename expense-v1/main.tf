4e630dc04312dc0e5989a37e34ca6a5f0ebd9f48
tanvighai <tanvighai02@gmail.com>
2023-11-15 21:32:32 -0500

    test commit

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
@@ -20,7 +20,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "backend"
@@ -40,7 +40,7 @@ resource "aws_route53_record" "backend" {
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0df7406ff2c2fb459"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "mysql"
